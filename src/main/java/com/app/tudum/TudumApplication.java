23ee2bb16712b58481c2a04923e3931242315cf8
drigoBarbosa <Rodrigobs8157@gmail.com>
2025-04-22 21:57:55 -0300

    Primeiro commit

---
+++ src/main/java/com/app/tudum/TudumApplication.java
@@ -0,0 +1,14 @@
+package com.app.tudum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudumApplication.class, args);
+	}
+
+}
+	
---
+++ src/test/java/com/app/tudum/TudumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tudum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
